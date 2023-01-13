2897799cb2de1f65ba41520432afe54f4db5b076
Ewerton Pereira <110136850+ewertonpereira@users.noreply.github.com>
2023-01-13 16:10:33 -0300

    update arrow buttons

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const img = document.getElementById('personal-photo')
+const personal_photo = document.getElementById('personal-photo')
+const bt1 = document.getElementById('bt1')
+const bt2 = document.getElementById('bt2')
+const bt3 = document.getElementById('bt3')
 
+
+bt1.addEventListener('click', () => {
+    personal_photo.style.transform='rotate(-360deg)';
+    bt1.style.visibility='hidden';
+    if(bt2.style.visibility == 'hidden' ){
+        bt3.style.visibility='visible';
+    }
+});
+
+bt2.addEventListener('click', () => {
+    personal_photo.style.transform='rotate(360deg)';
+    bt2.style.visibility='hidden';
+    if(bt1.style.visibility == 'hidden' ){
+        bt3.style.visibility='visible';
+    }
+});
+
+bt3.addEventListener('click', () =>{
+    bt1.style.visibility='visible';
+    bt2.style.visibility='visible';
+    bt3.style.visibility='hidden';
+});
